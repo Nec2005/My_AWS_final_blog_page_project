ef8e7c8f5b17620470bc250116548dde44039c76
Nec2005 <necatiunal2005@gmail.com>
2021-12-01 15:43:44 +0000

    data added

--- userdata.sh
+++ userdata.sh
@@ -3,13 +3,13 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-TOKEN=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-git clone https://$TOKEN@<YOUR GITHUB REPO URL>
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>
+TOKEN=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
+git clone https://$TOKEN@https://github.com/Nec2005/My_AWS_final_project.git
+cd /home/ubuntu/My_AWS_final_project
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>/src
+cd /home/ubuntu/My_AWS_final_project/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
