dc79086affda896fad4f2677ee56131c90d935cb
Nec2005 <necatiunal2005@gmail.com>
2022-07-11 22:20:56 +0100

    settings

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -85,10 +85,10 @@ WSGI_APPLICATION = 'cblog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'tomas_capstone', # database name in RDS is written here
+        'NAME': 'aws-capstone-RDS', # database name in RDS is written here
         'USER': 'admin', # database master username in RDS is written here
         'PASSWORD': config('PASSWORD'),
-        'HOST': 'capstone-rds.caauu1jlpbln.us-east-1.rds.amazonaws.com',  # database endpoint is written here
+        'HOST': 'aws-capstone-rds.caauu1jlpbln.us-east-1.rds.amazonaws.com',  # database endpoint is written here
         'PORT': '3306' # database port is written here
     }
 }
